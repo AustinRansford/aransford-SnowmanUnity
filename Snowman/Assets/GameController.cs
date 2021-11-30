a4fce74db1b36a6988c5b81a2786996f56efe615
AustinRansford <89941018+AustinRansford@users.noreply.github.com>
2021-11-29 18:21:41 -0800

    Work for milestone 5

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -5,30 +5,47 @@ using WordGuesser;
 
 public class GameController : MonoBehaviour
 {
-  public UnityEngine.UI.Text Message;
-  public UnityEngine.UI.Button StartButton;
-  public GameObject StartScreen; 
-  public GameObject PlayScreen;
-  private WordGuesser.WordGame GuessingGame;
-  public UnityEngine.UI.InputField PlayerGuess; 
-    public void StartGame()
-  {
-     this.GuessingGame = new WordGuesser.WordGame("apple", 5);
-     Debug.Log(this.GuessingGame.GetWord());
-     Debug.Log(this.GuessingGame.GetFullWord());
+    public UnityEngine.UI.Text Message;
+    public UnityEngine.UI.Button StartButton;
+    public GameObject StartScreen;
+    public GameObject PlayScreen;
+    private WordGuesser.WordGame GuessingGame;
+    public UnityEngine.UI.InputField PlayerGuess;
+    public GameObject GameOverScreen;
+    public GameObject GameWonScreen;
+    public void StartGame() 
+    {
+        this.GuessingGame = new WordGuesser.WordGame("apple", 5);
+        Debug.Log(this.GuessingGame.GetWord());
+        Debug.Log(this.GuessingGame.GetFullWord());
 
-      this.StartScreen.SetActive(false);
-      this.PlayScreen.SetActive(true); 
+        this.StartScreen.SetActive(false);
+        this.PlayScreen.SetActive(true);
+        this.GameOverScreen.SetActive(false);
 
-  }
-  public void OpenStartScreen()
-  {
-    this.StartScreen.SetActive(true); 
-      this.PlayScreen.SetActive(false);
-  }
-  public void SubmitGuess()
-  {
-   Debug.Log(this.GuessingGame.CheckGuess (PlayerGuess.text));
-   PlayerGuess.text = string.Empty; 
-  }
+    }
+    public void OpenStartScreen()
+    {
+        this.StartScreen.SetActive(true);
+        this.PlayScreen.SetActive(false);
+        this.GameOverScreen.SetActive(false);
+    }
+    public void SubmitGuess()
+    {
+        Debug.Log(this.GuessingGame.CheckGuess(PlayerGuess.text));
+        PlayerGuess.text = string.Empty;
+    }
+    public void IsGameOver()
+    {
+        this.GameOverScreen.SetActive(true);
+        this.PlayScreen.SetActive(false);
+        this.StartScreen.SetActive(false);
+    }
+    public void IsGameWon()
+    {
+      this.GameWonScreen.SetActive(true);
+        this.PlayScreen.SetActive(false);
+        this.StartScreen.SetActive(false);
+        this.GameOverScreen.SetActive(false);
+    }
 }
