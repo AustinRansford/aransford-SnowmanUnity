a79dba3fa81c0bd81754cb65e48355609dae9267
AustinRansford <89941018+AustinRansford@users.noreply.github.com>
2021-12-01 13:43:01 -0800

    milestone5

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -13,9 +13,17 @@ public class GameController : MonoBehaviour
     public UnityEngine.UI.InputField PlayerGuess;
     public GameObject GameOverScreen;
     public GameObject GameWonScreen;
+public UnityEngine.UI.Text Fullword;
+    public UnityEngine.UI.Text remaining2;
+    public UnityEngine.UI.Text getWordtext;
+    public UnityEngine.UI.Text hiddenWord;
+    public UnityEngine.UI.Text getGuessedLetterstext;
+    public UnityEngine.UI.Text checkGuess;
+
+
     public void StartGame() 
     {
-        this.GuessingGame = new WordGuesser.WordGame("apple", 5);
+        this.GuessingGame = new WordGuesser.WordGame("apple", 8);
         Debug.Log(this.GuessingGame.GetWord());
         Debug.Log(this.GuessingGame.GetFullWord());
 
@@ -32,17 +40,37 @@ public class GameController : MonoBehaviour
     }
     public void SubmitGuess()
     {
-        Debug.Log(this.GuessingGame.CheckGuess(PlayerGuess.text));
+        // Debug.Log(this.GuessingGame.CheckGuess(PlayerGuess.text));
+                hiddenWord.text = this.GuessingGame.CheckGuess(PlayerGuess.text);
+
         PlayerGuess.text = string.Empty;
+        getWordtext.text = this.GuessingGame.GetWord();
+        int remaining = this.GuessingGame.GetGuessLimit() - this.GuessingGame.GetIncorrectGuesses();
+        remaining2.text = $"Rem: {remaining}"; 
+
+        getGuessedLetterstext.text = this.GuessingGame.GetGuessedLetters();
+
+        if(this.GuessingGame.IsGameOver())
+        {
+            this.IsGameOver1();
+        }
+        else if (this.GuessingGame.IsGameWon())
+        {
+            this.IsGameWon1();
+        }
+
     }
-    public void IsGameOver()
+    public void IsGameOver1()
     {
+
+      this.GameWonScreen.SetActive(false);
         this.GameOverScreen.SetActive(true);
         this.PlayScreen.SetActive(false);
         this.StartScreen.SetActive(false);
     }
-    public void IsGameWon()
+    public void IsGameWon1()
     {
+        Fullword.text = this.GuessingGame.GetFullWord();
       this.GameWonScreen.SetActive(true);
         this.PlayScreen.SetActive(false);
         this.StartScreen.SetActive(false);
